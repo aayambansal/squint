a4158ad9e35fa384fe3ce768fc94042960f99b7e
Aayam Bansal <aayambansal@gmail.com>
2026-07-26 04:21:11 +0800

    release: 0.2.2

--- src/cli.tsx
+++ src/cli.tsx
@@ -14,7 +14,7 @@ import { composePrompt } from './prompt/brief.js'
 import { runAgent } from './runner/run.js'
 import { App } from './tui/App.js'
 
-const VERSION = '0.2.1'
+const VERSION = '0.2.2'
 
 const program = new Command()
 
